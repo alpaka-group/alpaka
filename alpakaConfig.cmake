83ddac55624edf38085a160aeebd046b7fd2c5ad
BenjaminW3 <benjaminworpitz@gmail.com>
2015-09-30 13:32:21 +0200

    disable the OpenMP 4 back-end by default
    The "omp declare target" pragma is missing around functions that could be called within kernels.
    GCC ignores this, because it does not support any non-CPU back-ends.
    However, ICC errors on this.

--- alpakaConfig.cmake
+++ alpakaConfig.cmake
@@ -88,7 +88,7 @@ OPTION(ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLE "Enable the threads CPU block threa
 OPTION(ALPAKA_ACC_CPU_B_SEQ_T_FIBERS_ENABLE "Enable the fibers CPU block thread accelerator" ON)
 OPTION(ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLE "Enable the OpenMP 2.0 CPU grid block accelerator" ON)
 OPTION(ALPAKA_ACC_CPU_B_SEQ_T_OMP2_ENABLE "Enable the OpenMP 2.0 CPU block thread accelerator" ON)
-OPTION(ALPAKA_ACC_CPU_BT_OMP4_ENABLE "Enable the OpenMP 4.0 CPU block and block thread accelerator" ON)
+OPTION(ALPAKA_ACC_CPU_BT_OMP4_ENABLE "Enable the OpenMP 4.0 CPU block and block thread accelerator" OFF)
 OPTION(ALPAKA_ACC_GPU_CUDA_ENABLE "Enable the CUDA GPU accelerator" ON)
 
 # Drop-down combo box in cmake-gui.
