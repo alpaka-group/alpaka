ba0dbad79e2e130da77df07667a7c4a7e8e88ad4
Axel Huebl <axel.huebl@plasma.ninja>
2018-10-03 22:19:19 +0200

    Mem Tests: Boost Predef
    Apply our boost predef work-around to two tests where it was
    missing.

--- test/unit/mem/p2p/src/P2P.cpp
+++ test/unit/mem/p2p/src/P2P.cpp
@@ -32,7 +32,7 @@
 #include <alpaka/test/queue/Queue.hpp>
 #include <alpaka/test/mem/view/ViewTest.hpp>
 
-#include <boost/predef.h>
+#include <alpaka/core/BoostPredef.hpp>
 #if BOOST_COMP_CLANG
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wunused-parameter"
