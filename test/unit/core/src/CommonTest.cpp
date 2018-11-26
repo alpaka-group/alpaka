8df51a8d5a569ceedce3ca092887d6bac435916f
Benjamin Worpitz <benjaminworpitz@gmail.com>
2018-11-26 22:01:09 +0100

    Extend CommonTest

--- test/unit/core/src/CommonTest.cpp
+++ test/unit/core/src/CommonTest.cpp
@@ -29,33 +29,42 @@
 TEST_CASE("printDefines", "[core]")
 {
 #if BOOST_LANG_CUDA
-    std::cout << "BOOST_LANG_CUDA" << std::endl;
+    std::cout << "BOOST_LANG_CUDA:" << BOOST_LANG_CUDA << std::endl;
 #endif
 #if BOOST_LANG_HIP
-    std::cout << "BOOST_LANG_HIP" << std::endl;
+    std::cout << "BOOST_LANG_HIP:" << BOOST_LANG_HIP << std::endl;
 #endif
 #if BOOST_ARCH_PTX
-    std::cout << "BOOST_ARCH_PTX" << std::endl;
+    std::cout << "BOOST_ARCH_PTX:" << BOOST_ARCH_PTX << std::endl;
 #endif
 #if BOOST_ARCH_HSA
-    std::cout << "BOOST_ARCH_HSA" << std::endl;
+    std::cout << "BOOST_ARCH_HSA:" << BOOST_ARCH_HSA << std::endl;
 #endif
 #if BOOST_COMP_NVCC
-    std::cout << "BOOST_COMP_NVCC" << std::endl;
+    std::cout << "BOOST_COMP_NVCC:" << BOOST_COMP_NVCC << std::endl;
 #endif
 #if BOOST_COMP_HCC
-    std::cout << "BOOST_COMP_HCC" << std::endl;
+    std::cout << "BOOST_COMP_HCC:" << BOOST_COMP_HCC << std::endl;
 #endif
 #if BOOST_COMP_CLANG
-    std::cout << "BOOST_COMP_CLANG" << std::endl;
+    std::cout << "BOOST_COMP_CLANG:" << BOOST_COMP_CLANG << std::endl;
 #endif
 #if BOOST_COMP_GNUC
-    std::cout << "BOOST_COMP_GNUC" << std::endl;
+    std::cout << "BOOST_COMP_GNUC:" << BOOST_COMP_GNUC << std::endl;
 #endif
 #if BOOST_COMP_MSVC
-    std::cout << "BOOST_COMP_MSVC" << std::endl;
+    std::cout << "BOOST_COMP_MSVC:" << BOOST_COMP_MSVC << std::endl;
 #endif
 #if BOOST_COMP_CLANG_CUDA
-    std::cout << "BOOST_COMP_CLANG_CUDA" << std::endl;
+    std::cout << "BOOST_COMP_CLANG_CUDA:" << BOOST_COMP_CLANG_CUDA << std::endl;
+#endif
+#if BOOST_LIB_STD_GNU
+    std::cout << "BOOST_LIB_STD_GNU:" << BOOST_LIB_STD_GNU << std::endl;
+#endif
+#if BOOST_LIB_STD_CXX
+    std::cout << "BOOST_LIB_STD_CXX:" << BOOST_LIB_STD_CXX << std::endl;
+#endif
+#if BOOST_LIB_STD_DINKUMWARE
+    std::cout << "BOOST_LIB_STD_DINKUMWARE:" << BOOST_LIB_STD_DINKUMWARE << std::endl;
 #endif
 }
