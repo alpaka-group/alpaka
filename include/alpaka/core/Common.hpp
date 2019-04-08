0318d5e85f47e80340d00adee7e8badaeda01c04
Benjamin Worpitz <benjaminworpitz@gmail.com>
2019-04-08 10:17:10 +0200

    Make macros singleline

--- include/alpaka/core/Common.hpp
+++ include/alpaka/core/Common.hpp
@@ -77,14 +77,11 @@
 //!
 //! WARNING: Only use this method if there is no other way.
 //! Most cases can be solved by #if BOOST_ARCH_PTX or #if BOOST_LANG_CUDA.
-#if (BOOST_LANG_CUDA && !BOOST_COMP_CLANG_CUDA) \
-  || BOOST_LANG_HIP
+#if (BOOST_LANG_CUDA && !BOOST_COMP_CLANG_CUDA) || BOOST_LANG_HIP
     #if BOOST_COMP_MSVC
-        #define ALPAKA_NO_HOST_ACC_WARNING\
-            __pragma(hd_warning_disable)
+        #define ALPAKA_NO_HOST_ACC_WARNING __pragma(hd_warning_disable)
     #else
-        #define ALPAKA_NO_HOST_ACC_WARNING\
-            _Pragma("hd_warning_disable")
+        #define ALPAKA_NO_HOST_ACC_WARNING _Pragma("hd_warning_disable")
     #endif
 #else
     #define ALPAKA_NO_HOST_ACC_WARNING
