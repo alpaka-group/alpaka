f5661b79214b08b8574cc2ac449b5fa495c4895a
Jakob <jakob.krude@hotmail.com>
2019-10-18 16:49:21 +0200

    Added trait specialisation for double.
    Int is also supported in cuda/hip, so fmod is used for double type.

--- include/alpaka/math/abs/AbsCudaBuiltIn.hpp
+++ include/alpaka/math/abs/AbsCudaBuiltIn.hpp
@@ -69,6 +69,21 @@ namespace alpaka
                     //boost::ignore_unused(abs);
                     return ::abs(arg);
                 }
+            }
+            //! The CUDA built in abs double specialization.
+            template<>
+            struct Abs<
+                AbsCudaBuiltIn,
+                double>
+            {
+                __device__ static auto abs(
+                    AbsCudaBuiltIn const & abs_ctx,
+                    TArg const & arg)
+                -> decltype(::fabs(arg))
+                {
+                    alpaka::ignore_unused(abs_ctx);
+                    return ::fabs(arg);
+                }
             };
             //! The CUDA built in abs float specialization.
             template<>
