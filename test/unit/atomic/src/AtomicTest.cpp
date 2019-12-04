0e670e8fc41a9989b3465ecfb574b8ff5f877972
Jeffrey Kelling <j.kelling@hzdr.de>
2019-12-04 20:34:26 +0100

    omp4: disable AtomicTest, because of LLVM NVPTX bug:
    https://bugs.llvm.org/show_bug.cgi?id=44219

--- test/unit/atomic/src/AtomicTest.cpp
+++ test/unit/atomic/src/AtomicTest.cpp
@@ -973,9 +973,23 @@ struct TestAtomicOperations
     }
 };
 
-using TestAccs = alpaka::test::acc::EnabledAccs<
-    alpaka::dim::DimInt<1u>,
-    std::size_t>;
+template<typename B>
+struct not_omp4 : public std::integral_constant<
+    bool,
+    !bool(std::is_same<
+        alpaka::acc::AccCpuOmp4<
+            alpaka::dim::DimInt<1u>,
+            std::size_t>,
+        B>::value
+    )>
+{};
+
+using TestAccs = alpaka::meta::Filter<
+    alpaka::test::acc::EnabledAccs<
+        alpaka::dim::DimInt<1u>,
+        std::size_t>,
+    not_omp4
+    >;
 
 //-----------------------------------------------------------------------------
 TEMPLATE_LIST_TEST_CASE( "atomicOperationsWorking", "[atomic]", TestAccs)
