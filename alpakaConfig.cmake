c26dbc3804c664c236ad06055a004c32ae9c5a46
Benjamin Worpitz <benjaminworpitz@gmail.com>
2020-02-16 23:12:13 +0100

    Remove setting cmake policies (#933)
    After we increased the CMAKE_MINIMUM_REQUIRED version we can now remove
    setting older policies because all of them are now implicitly set to new.

--- alpakaConfig.cmake
+++ alpakaConfig.cmake
@@ -14,16 +14,6 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 3.15)
 
 ################################################################################
-# CMake policies
-#
-# Search in <PackageName>_ROOT:
-#   https://cmake.org/cmake/help/v3.12/policy/CMP0074.html
-
-if(POLICY CMP0074)
-    cmake_policy(SET CMP0074 NEW)
-endif()
-
-################################################################################
 # alpaka.
 
 # Return values.
