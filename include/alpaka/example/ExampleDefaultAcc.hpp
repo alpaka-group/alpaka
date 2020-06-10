b393d2bf95c128a618c0768c7f190fa2200d16ba
Jeffrey Kelling <j.kelling@hzdr.de>
2020-06-10 21:02:24 +0200

    ExampleDefaultAcc: style fixes

--- include/alpaka/example/ExampleDefaultAcc.hpp
+++ include/alpaka/example/ExampleDefaultAcc.hpp
@@ -26,25 +26,25 @@ namespace alpaka
         //! Alias for the default accelerator used by examples. From a list of
         //! all accelerators the first one which is enabled is chosen.
         //! AccCpuSerial is selected last.
-        template<class A, class B>
+        template<class TDim, class TIdx>
 #if defined(ALPAKA_ACC_GPU_CUDA_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccGpuCudaRt<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccGpuCudaRt<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_GPU_HIP_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccGpuHipRt<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccGpuHipRt<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp2Blocks<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp2Blocks<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuTbbBlocks<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuTbbBlocks<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_SEQ_T_FIBERS_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuFibers<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuFibers<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_SEQ_T_OMP2_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp2Threads<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp2Threads<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuThreads<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuThreads<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_BT_OMP4_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp4<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuOmp4<TDim,TIdx>;
 #elif defined(ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED)
-        using ExampleDefaultAcc = alpaka::acc::AccCpuSerial<A,B>;
+        using ExampleDefaultAcc = alpaka::acc::AccCpuSerial<TDim,TIdx>;
 #else
         class ExampleDefaultAcc;
         #warning "No supported backend selected."
