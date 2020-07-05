b2a80b16945ed0ce7a4c4bfe70f3a096c8e33f85
Benjamin Worpitz <benjaminworpitz@gmail.com>
2020-07-05 11:49:28 +0200

    Add support for CUDA 11

--- script/install_cuda.sh
+++ script/install_cuda.sh
@@ -64,8 +64,13 @@ then
         ALPAKA_CUDA_PKG_DEB_NAME=cuda-repo-ubuntu1804-10-2-local
         ALPAKA_CUDA_PKG_FILE_NAME="${ALPAKA_CUDA_PKG_DEB_NAME}"-10.2.89-440.33.01_1.0-1_amd64.deb
         ALPAKA_CUDA_PKG_FILE_PATH=http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/${ALPAKA_CUDA_PKG_FILE_NAME}
+    elif [ "${ALPAKA_CUDA_VERSION}" == "11.0" ]
+    then
+        ALPAKA_CUDA_PKG_DEB_NAME=cuda-repo-ubuntu1804-11-0-local
+        ALPAKA_CUDA_PKG_FILE_NAME="${ALPAKA_CUDA_PKG_DEB_NAME}"_11.0.2-450.51.05-1_amd64.deb
+        ALPAKA_CUDA_PKG_FILE_PATH=http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/${ALPAKA_CUDA_PKG_FILE_NAME}
     else
-        echo CUDA versions other than 9.0, 9.1, 9.2, 10.0, 10.1 and 10.2 are not currently supported on linux!
+        echo CUDA versions other than 9.0, 9.1, 9.2, 10.0, 10.1, 10.2 and 11.0 are not currently supported on linux!
     fi
     if [ -z "$(ls -A ${ALPAKA_CI_CUDA_DIR})" ]
     then
@@ -77,9 +82,14 @@ then
     travis_retry sudo apt-get -y --quiet update
 
     # Install CUDA
-    # Currently we do not install CUDA fully: sudo apt-get --quiet -y install cuda
-    # We only install the minimal packages. Because of our manual partial installation we have to create a symlink at /usr/local/cuda
-    sudo apt-get -y --quiet --allow-unauthenticated --no-install-recommends install cuda-core-"${ALPAKA_CUDA_VERSION}" cuda-cudart-"${ALPAKA_CUDA_VERSION}" cuda-cudart-dev-"${ALPAKA_CUDA_VERSION}" cuda-curand-"${ALPAKA_CUDA_VERSION}" cuda-curand-dev-"${ALPAKA_CUDA_VERSION}"
+    if [ "${ALPAKA_CUDA_VERSION}" == "11.0" ]
+    then
+      sudo apt-get -y --quiet --allow-unauthenticated --no-install-recommends install cuda-compiler-"${ALPAKA_CUDA_VERSION}" cuda-cudart-"${ALPAKA_CUDA_VERSION}" cuda-cudart-dev-"${ALPAKA_CUDA_VERSION}" libcurand-"${ALPAKA_CUDA_VERSION}" libcurand-dev-"${ALPAKA_CUDA_VERSION}"
+    else
+      # Currently we do not install CUDA fully: sudo apt-get --quiet -y install cuda
+      # We only install the minimal packages. Because of our manual partial installation we have to create a symlink at /usr/local/cuda
+      sudo apt-get -y --quiet --allow-unauthenticated --no-install-recommends install cuda-core-"${ALPAKA_CUDA_VERSION}" cuda-cudart-"${ALPAKA_CUDA_VERSION}" cuda-cudart-dev-"${ALPAKA_CUDA_VERSION}" cuda-curand-"${ALPAKA_CUDA_VERSION}" cuda-curand-dev-"${ALPAKA_CUDA_VERSION}"
+    fi
     sudo ln -s /usr/local/cuda-"${ALPAKA_CUDA_VERSION}" /usr/local/cuda
 
     if [ "${ALPAKA_CUDA_COMPILER}" == "clang" ]
