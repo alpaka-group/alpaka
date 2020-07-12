37c29339b036581029457a7899767b6521d6fedf
Axel Huebl <axel.huebl@plasma.ninja>
2020-07-11 21:46:52 -0700

    Boost CXXFLAGS: C++11

--- script/install_boost.sh
+++ script/install_boost.sh
@@ -40,7 +40,7 @@ then
     ${CXX} -v
     which "${CC}"
     ${CC} -v
-    # export CXXFLAGS="${CXXFLAGS} -std=c++11"
+    export CXXFLAGS="-std=c++11"
     (cd "${BOOST_ROOT}"; sudo ./bootstrap.sh --with-toolset="cxx" || cat bootstrap.log)
 else
     (cd "${BOOST_ROOT}"; sudo ./bootstrap.sh --with-toolset="${CC}")
