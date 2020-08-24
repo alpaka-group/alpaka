e623c1e7a75e381ed9cda133dba1257c53bd09b7
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2020-08-24 09:43:22 +0200

    Remove placement new call from BlockSharedMemStMemberImpl
    As discussed in #1128, this is not consistent with other implementations of shared memory
    As a side product, when out of memory this allows the debug assert to shoot before the segfault happens
    
    Co-authored-by: Jeffrey Kelling <j.kelling@hzdr.de>

--- include/alpaka/block/shared/st/BlockSharedMemStMember.hpp
+++ include/alpaka/block/shared/st/BlockSharedMemStMember.hpp
@@ -59,8 +59,6 @@ namespace alpaka
                         void alloc() const
                         {
                             m_allocdBytes = allocPitch<T>();
-                            uint8_t* buf = &m_mem[m_allocdBytes];
-                            new (buf) T();
                             m_allocdBytes += sizeof(T);
 #if (defined ALPAKA_DEBUG_OFFLOAD_ASSUME_HOST) && (! defined NDEBUG)
                             ALPAKA_ASSERT(m_allocdBytes <= m_capacity);
