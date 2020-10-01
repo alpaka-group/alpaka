49828640cca6c114b10a1fa9738ec49d3e61bd03
Benjamin Worpitz <benjaminworpitz@gmail.com>
2020-10-01 21:30:48 +0200

    First-class CMake CUDA support
    * removes support for CMake 3.15.7, 3.16.8, 3.17.3 and enforces 3.18.0+
    * removes tests for CUDA builds using libc++ instead of libstdc++ (I was not able to get this working)
    
    Breaking changes:
    * If you want to select the CUDA compiler explicitly, you can specify `CMAKE_CUDA_COMPILER` or set `CUDACXX` to the path to the CUDA compiler e.g.. nvcc or clang++. The default seems to be nvcc.
    * If you selected `CMAKE_CUDA_COMPILER: clang++` and want to specify a specific CUDA toolkit different from the default that CMake takes, you have to specify `CUDAToolkit_ROOT`.
    * `ALPAKA_CUDA_NVCC_SEPARABLE_COMPILATION` has been replaced by `CMAKE_CUDA_SEPARABLE_COMPILATION`
    * `ALPAKA_CUDA_ARCH="35;72"` has ben replaced by `CMAKE_CUDA_ARCHITECTURES="35;72"`

--- script/run.sh
+++ script/run.sh
@@ -59,25 +59,30 @@ fi
 # CUDA
 if [ "${ALPAKA_CI_INSTALL_CUDA}" == "ON" ]
 then
-    : "${ALPAKA_CUDA_VERSION?'ALPAKA_CUDA_VERSION must be specified'}"
+    : "${ALPAKA_CI_CUDA_VERSION?'ALPAKA_CI_CUDA_VERSION must be specified'}"
 
     if [ "$ALPAKA_CI_OS_NAME" = "Linux" ]
     then
         # CUDA
-        export PATH=/usr/local/cuda-${ALPAKA_CUDA_VERSION}/bin:$PATH
-        export LD_LIBRARY_PATH=/usr/local/cuda-${ALPAKA_CUDA_VERSION}/lib64:${LD_LIBRARY_PATH}
+        export PATH=/usr/local/cuda-${ALPAKA_CI_CUDA_VERSION}/bin:$PATH
+        export LD_LIBRARY_PATH=/usr/local/cuda-${ALPAKA_CI_CUDA_VERSION}/lib64:${LD_LIBRARY_PATH}
         # We have to explicitly add the stub libcuda.so to CUDA_LIB_PATH because the real one would be installed by the driver (which we can not install).
         export CUDA_LIB_PATH=/usr/local/cuda/lib64/stubs/
 
-        if [ "${ALPAKA_CUDA_COMPILER}" == "nvcc" ]
+        if [ "${CMAKE_CUDA_COMPILER}" == "nvcc" ]
         then
             which nvcc
             nvcc -V
         fi
     elif [ "$ALPAKA_CI_OS_NAME" = "Windows" ]
     then
-        export PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${ALPAKA_CUDA_VERSION}\bin":$PATH
-        export CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v${ALPAKA_CUDA_VERSION}"
+        export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${ALPAKA_CI_CUDA_VERSION}/bin":$PATH
+        export CUDA_PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${ALPAKA_CI_CUDA_VERSION}"
+
+        which nvcc
+        nvcc -V
+
+        export CUDA_PATH_V${ALPAKA_CI_CUDA_VERSION/./_}="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${ALPAKA_CI_CUDA_VERSION}"
     fi
 fi
 
@@ -119,13 +124,11 @@ then
         then
             export CMAKE_CXX_FLAGS=
         fi
-        CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
 
-        if [ -z "${CMAKE_EXE_LINKER_FLAGS+x}" ]
+        if [ "${CXX}" == "clang++" ]
         then
-            export CMAKE_EXE_LINKER_FLAGS=
+            CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
         fi
-        CMAKE_EXE_LINKER_FLAGS="${CMAKE_EXE_LINKER_FLAGS} -lc++ -lc++abi"
     fi
 
     if [ "${CXX}" == "icpc" ]
@@ -137,17 +140,16 @@ then
 
     which "${CXX}"
     ${CXX} -v
+fi
 
+if [ "$ALPAKA_CI_OS_NAME" = "Linux" ]
+then
     source ./script/prepare_sanitizers.sh
 fi
 
 if [ "$ALPAKA_CI_OS_NAME" = "Windows" ]
 then
     : ${ALPAKA_CI_CL_VER?"ALPAKA_CI_CL_VER must be specified"}
-
-    # Use the 64 bit compiler
-    # FIXME: Path not found but does not seem to be necessary anymore
-    #"./C/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat" amd64
 
     # Add msbuild to the path
     if [ "$ALPAKA_CI_CL_VER" = "2017" ]
