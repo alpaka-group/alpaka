17bdf1820c5fd5377e17ca550c6dd0f5fada6b81
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-10-27 13:58:40 +0100

    add explicitly spelled atomic functions that call the generic atomicOp
    This simplifies the user side API. See #1005.

--- include/alpaka/atomic/Traits.hpp
+++ include/alpaka/atomic/Traits.hpp
@@ -12,6 +12,7 @@
 #include <alpaka/core/Positioning.hpp>
 #include <alpaka/core/Common.hpp>
 #include <alpaka/core/Concepts.hpp>
+#include <alpaka/atomic/Op.hpp>
 
 #include <type_traits>
 
@@ -141,5 +142,260 @@ namespace alpaka
                 addr,
                 compare,
                 value);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic add operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicAdd(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Add>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic sub operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicSub(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Sub>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic min operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicMin(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Min>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic max operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicMax(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Max>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic exchange operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicExch(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Exch>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic increment operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicInc(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Inc>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic decrement operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicDec(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Dec>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic and operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicAnd(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::And>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic or operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicOr(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Or>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic xor operation.
+    //!
+    //! \tparam T The value type.
+    //! \tparam TAtomic The atomic implementation type.
+    //! \param addr The value to change atomically.
+    //! \param value The value used in the atomic operation.
+    //! \param atomic The atomic implementation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicXor(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Xor>(atomic, addr, value, hier);
+    }
+
+    //-----------------------------------------------------------------------------
+    //! Executes an atomic compare-and-swap operation.
+    //!
+    //! \tparam TAtomic The atomic implementation type.
+    //! \tparam T The value type.
+    //! \param atomic The atomic implementation.
+    //! \param addr The value to change atomically.
+    //! \param compare The comparison value used in the atomic operation.
+    //! \param value The value used in the atomic operation.
+    ALPAKA_NO_HOST_ACC_WARNING
+        template<
+        typename TAtomic,
+        typename T,
+        typename THierarchy = hierarchy::Grids>
+        ALPAKA_FN_HOST_ACC auto atomicCas(
+            TAtomic const& atomic,
+            T* const addr,
+            T const& compare,
+            T const& value,
+            THierarchy const& hier = THierarchy())
+        -> T
+    {
+        return atomicOp<op::Cas>(atomic, addr, compare, value, hier);
     }
 }
