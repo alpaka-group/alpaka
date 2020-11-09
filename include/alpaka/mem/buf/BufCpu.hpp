8cda764accf7a4788cac235921a1b981a23ab7b2
René Widera <r.widera@hzdr.de>
2020-11-09 11:23:36 +0100

    fix bug in `isPinned()` and `pin()`
    With the renaming of the namespaces in #1173 some calls to the traits
    are broken.
    Instead of calling the trait the local member function is called again.

--- include/alpaka/mem/buf/BufCpu.hpp
+++ include/alpaka/mem/buf/BufCpu.hpp
@@ -464,7 +464,7 @@ namespace alpaka
                 BufCpu<TElem, TDim, TIdx> & buf)
             -> void
             {
-                unpin(*buf.m_spBufCpuImpl.get());
+                alpaka::unpin(*buf.m_spBufCpuImpl.get());
             }
         };
         //#############################################################################
@@ -514,7 +514,7 @@ namespace alpaka
                 BufCpu<TElem, TDim, TIdx> const & buf)
             -> bool
             {
-                return isPinned(*buf.m_spBufCpuImpl.get());
+                return alpaka::isPinned(*buf.m_spBufCpuImpl.get());
             }
         };
         //#############################################################################
