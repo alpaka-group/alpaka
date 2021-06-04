b8ad5564710290b817b28b7743c435e71d508a1d
René Widera <r.widera@hzdr.de>
2021-06-04 09:13:10 +0200

    CI: test MSVC 2019+nvcc CUDA 11.2
    Remove Visual Studio 2019 CUDA 11.1 tests with CUDA 11.2 to workaround #1331.

--- script/install_cuda.sh
+++ script/install_cuda.sh
@@ -130,10 +130,18 @@ then
     then
         ALPAKA_CUDA_PKG_FILE_NAME=cuda_11.1.1_456.81_win10.exe
         ALPAKA_CUDA_PKG_FILE_PATH=https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/${ALPAKA_CUDA_PKG_FILE_NAME}
+    elif [ "${ALPAKA_CUDA_VERSION}" == "11.2" ]
+    then
+        ALPAKA_CUDA_PKG_FILE_NAME=cuda_11.2.2_461.33_win10.exe
+        ALPAKA_CUDA_PKG_FILE_PATH=https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/${ALPAKA_CUDA_PKG_FILE_NAME}
+    elif [ "${ALPAKA_CUDA_VERSION}" == "11.3" ]
+    then
+        ALPAKA_CUDA_PKG_FILE_NAME=cuda_11.3.1_465.89_win10.exe
+        ALPAKA_CUDA_PKG_FILE_PATH=https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/${ALPAKA_CUDA_PKG_FILE_NAME}
     else
-        echo CUDA versions other than 10.0, 10.1, 10.2, 11.0 and 11.1 are not currently supported on Windows!
+        echo CUDA versions other than 10.0, 10.1, 10.2, 11.0, 11.1, 11.2 and 11.3 are not currently supported on Windows!
     fi
 
     curl -L -o cuda_installer.exe ${ALPAKA_CUDA_PKG_FILE_PATH}
-    ./cuda_installer.exe -s "nvcc_${ALPAKA_CUDA_VERSION}" "curand_dev_${ALPAKA_CUDA_VERSION}" "cudart_${ALPAKA_CUDA_VERSION}"
+    ./cuda_installer.exe -s "nvcc_${ALPAKA_CUDA_VERSION}" "curand_dev_${ALPAKA_CUDA_VERSION}" "cudart_${ALPAKA_CUDA_VERSION}" "visual_studio_integration_${ALPAKA_CUDA_VERSION}"
 fi
