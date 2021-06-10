601f142c421da1073398ecbf8f0062be96515489
René Widera <r.widera@hzdr.de>
2021-06-10 09:24:35 +0200

    HIP: fix compile errors
    Because using native clang for HIP which is handling `-Wall` correctly some
    errors popping up.

--- include/alpaka/core/Common.hpp
+++ include/alpaka/core/Common.hpp
@@ -44,7 +44,7 @@
 //! ALPAKA_FN_HOST_ACC function_declaration()
 //! WARNING: Only use this method if there is no other way.
 //! Most cases can be solved by #if BOOST_ARCH_PTX or #if BOOST_LANG_CUDA.
-#if(BOOST_LANG_CUDA && !BOOST_COMP_CLANG_CUDA) || BOOST_LANG_HIP
+#if(BOOST_LANG_CUDA && !BOOST_COMP_CLANG_CUDA)
 #    if BOOST_COMP_MSVC || defined(BOOST_COMP_MSVC_EMULATED)
 #        define ALPAKA_NO_HOST_ACC_WARNING __pragma(hd_warning_disable)
 #    else
