2b90b2481f268e11966668a9d2cab5158520ac72
Simeon Ehrig <s.ehrig@hzdr.de>
2021-07-27 13:43:43 +0000

    Fix bug, if alpaka_add_library() and Clang as CUDA compiler is used together

--- cmake/addLibrary.cmake
+++ cmake/addLibrary.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright 2015-2019 Benjamin Worpitz, Maximilian Knespel
+# Copyright 2015-2021 Benjamin Worpitz, Maximilian Knespel, Simeon Ehrig
 #
 # This file is part of alpaka.
 #
@@ -94,10 +94,10 @@ MACRO(ALPAKA_ADD_LIBRARY libraryName)
             ENDFOREACH()
             ADD_LIBRARY(
                 ${libraryName}
-                ${sourceFileNames}
                 ${libraryType}
                 ${excludeFromAll}
                 ${optionArguments}
+                ${sourceFileNames}
             )
         ELSE()
             FOREACH( _file ${ARGN} )
