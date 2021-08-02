627ad71d8e863a968d1f411d8074b839d11f2272
Simeon Ehrig <s.ehrig@hzdr.de>
2021-08-02 11:55:59 +0000

    Fix bug, if alpaka_add_library() and Clang as CUDA compiler is used together

--- cmake/addLibrary.cmake
+++ cmake/addLibrary.cmake
@@ -94,10 +94,10 @@ MACRO(ALPAKA_ADD_LIBRARY libraryName)
 
         ADD_LIBRARY(
             ${libraryName}
-            ${sourceFileNames}
             ${libraryType}
             ${excludeFromAll}
             ${optionArguments}
+            ${sourceFileNames}
             )
     ELSEIF( ALPAKA_ACC_GPU_HIP_ENABLE )
         FOREACH( _file ${ARGN} )
