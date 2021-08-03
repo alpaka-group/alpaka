751901a90cf26f197eacc89fd8940fb8d1306d57
René Widera <r.widera@hzdr.de>
2021-08-03 09:51:19 +0200

    ROCm container update outdated keys
    The keys for ROCm apt reposetories are outdated and must be updated to
    be able to install additional ROCm dependencies.

--- script/install_hip.sh
+++ script/install_hip.sh
@@ -16,5 +16,11 @@ source ./script/set.sh
 
 : "${ALPAKA_CI_HIP_ROOT_DIR?'ALPAKA_CI_HIP_ROOT_DIR must be specified'}"
 
+travis_retry apt-get -y --quiet update
+travis_retry apt-get -y --quiet wget gnupg2
+# AMD container keys are outdated and must be updated
+wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
+travis_retry apt-get -y --quiet update
+
 # AMD container are not shipped with rocrand/hiprand
 travis_retry sudo apt-get -y --quiet install rocrand
