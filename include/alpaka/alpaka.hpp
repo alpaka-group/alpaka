5062ec492af4713ecac880035d81e11171d9e784
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-10-26 18:44:14 +0200

    add Accessor for views and buffers
    * add access mode tags for read, write and read-write access
    * add readAccess()/writeAccess()/access()
    * only allow writing via WriteAccess accessors
    * add Accessor specific tests
    * add example for an accessor with a projection
    * allow constraining Accessors
    * add BuildAccessor customization point
    * view accessors are separate from generic Accessor functionality
    * add MemoryHandle trait to query memory handle of an accessor
    * add BufferAccessor convenience alias
    * add brief documentation

--- include/alpaka/alpaka.hpp
+++ include/alpaka/alpaka.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2019 Axel Huebl, Benjamin Worpitz, Erik Zenker, Matthias Werner, René Widera
+/* Copyright 2019-2021 Axel Huebl, Benjamin Worpitz, Erik Zenker, Matthias Werner, René Widera, Bernhard Manfred Gruber
  *
  * This file is part of alpaka.
  *
@@ -131,7 +131,9 @@
 #include <alpaka/mem/fence/MemFenceOmp5.hpp>
 #include <alpaka/mem/fence/MemFenceUniformCudaHipBuiltIn.hpp>
 #include <alpaka/mem/fence/Traits.hpp>
+#include <alpaka/mem/view/Accessor.hpp>
 #include <alpaka/mem/view/Traits.hpp>
+#include <alpaka/mem/view/ViewAccessor.hpp>
 #include <alpaka/mem/view/ViewCompileTimeArray.hpp>
 #include <alpaka/mem/view/ViewPlainPtr.hpp>
 #include <alpaka/mem/view/ViewStdArray.hpp>
