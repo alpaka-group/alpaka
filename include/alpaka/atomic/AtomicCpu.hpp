3d37d29501ee0a2675a45da4c228e68f013dd136
Felice Pantaleo <felice.pantaleo@cern.ch>
2021-11-17 18:25:06 +0100

    Add a switch at compile time for choosing the atomic

---
+++ include/alpaka/atomic/AtomicCpu.hpp
@@ -0,0 +1,26 @@
+/* Copyright 2021 Andrea Bocci, Felice Pantaleo
+ *
+ * This file is part of alpaka.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#ifndef ALPAKA_DISABLE_ATOMIC_ATOMICREF
+#    include <alpaka/atomic/AtomicAtomicRef.hpp>
+#else
+#    include <alpaka/atomic/AtomicStdLibLock.hpp>
+#endif // ALPAKA_DISABLE_ATOMIC_ATOMICREF
+
+namespace alpaka
+{
+#ifndef ALPAKA_DISABLE_ATOMIC_ATOMICREF
+    using AtomicCpu = AtomicAtomicRef;
+#else
+    using AtomicCpu = AtomicStdLibLock<16>;
+#endif // ALPAKA_DISABLE_ATOMIC_ATOMICREF
+
+} // namespace alpaka
