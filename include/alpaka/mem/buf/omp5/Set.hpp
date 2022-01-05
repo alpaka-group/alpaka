ff085af079b57976fbeb6af3e3ad6a7dceb82b30
Andrea Bocci <andrea.bocci@cern.ch>
2022-01-05 10:13:45 +0100

    Implement OpenMP memset copies of zero-dimensional (scalar) buffers
    Signed-off-by: Andrea Bocci <andrea.bocci@cern.ch>

--- include/alpaka/mem/buf/omp5/Set.hpp
+++ include/alpaka/mem/buf/omp5/Set.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2019 Benjamin Worpitz, Erik Zenker, Matthias Werner
+/* Copyright 2022 Benjamin Worpitz, Erik Zenker, Matthias Werner, Andrea Bocci
  *
  * This file is part of Alpaka.
  *
@@ -84,6 +84,31 @@ namespace alpaka
                     reinterpret_cast<std::uint8_t*>(alpaka::getPtrNative(view)),
                     byteExtent,
                     pitch);
+            }
+        };
+        //! The OMP5 device scalar memory set trait specialization.
+        template<>
+        struct CreateTaskMemset<DimInt<0u>, DevOmp5>
+        {
+            template<typename TExtent, typename TView>
+            ALPAKA_FN_HOST static auto createTaskMemset(TView& view, std::uint8_t const& byte, TExtent const& extent)
+            {
+                using Idx = typename alpaka::traits::IdxType<TExtent>::type;
+                using Dim1D = DimInt<1u>;
+                using Vec1D = Vec<Dim1D, Idx>;
+
+                static_assert(Dim<TView>::value == 0u, "The view is required to have dimensionality 0!");
+                static_assert(Dim<TExtent>::value == 0u, "The extent is required to have dimensionality 0!");
+
+                alpaka::ignore_unused(extent);
+
+                return createTaskKernel<AccOmp5<Dim1D, Idx>>(
+                    WorkDivMembers<Dim1D, Idx>(Vec1D::zeros(), Vec1D::zeros(), Vec1D::zeros()),
+                    MemSetKernel(),
+                    byte,
+                    reinterpret_cast<std::uint8_t*>(alpaka::getPtrNative(view)),
+                    Vec1D(sizeof(Elem<TView>)),
+                    Vec1D::zeros());
             }
         };
     } // namespace traits
