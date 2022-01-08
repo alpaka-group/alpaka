74f9bc69ae1652a323c67f562fba7f8b508576d8
Jeffrey Kelling <j.kelling@hzdr.de>
2022-01-08 12:19:23 +0100

    OAcc CI: Rm fixed CI block size from code:
    * rely on ALPAKA_OFFLOAD_MAX_BLOCK_SIZE being set properly
    * oacc: gcc 10 -> 11
    * oacc: nvhpc 21.9 -> 21.11
    * omp5: gcc 10 -> 11, do run test, not analysis
    * oacc,omp5: disable some tests for GCC(oacc,omp5) in general

--- include/alpaka/acc/AccOacc.hpp
+++ include/alpaka/acc/AccOacc.hpp
@@ -116,15 +116,9 @@ namespace alpaka
             {
                 alpaka::ignore_unused(dev);
 
-#    ifdef ALPAKA_CI
-                auto const blockThreadCountMax(
-                    alpaka::core::clipCast<TIdx>(std::min(static_cast<size_t>(2u), oaccMaxWorkerNum)));
-                auto const gridBlockCountMax(
-                    alpaka::core::clipCast<TIdx>(std::min(static_cast<size_t>(2u), oaccMaxGangNum)));
-#    else
                 auto const blockThreadCountMax(alpaka::core::clipCast<TIdx>(oaccMaxWorkerNum));
                 auto const gridBlockCountMax(alpaka::core::clipCast<TIdx>(oaccMaxGangNum));
-#    endif
+
                 return {// m_multiProcessorCount
                         static_cast<TIdx>(gridBlockCountMax),
                         // m_gridBlockExtentMax
