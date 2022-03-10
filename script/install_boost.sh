e88749e055d8008357fc40ed0f8c71fc8aa70ce4
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-03-10 18:06:25 +0100

    retry downloading boost

--- script/install_boost.sh
+++ script/install_boost.sh
@@ -36,7 +36,8 @@ then
     BOOST_BASE_VERSION=$(echo -n ${ALPAKA_CI_BOOST_BRANCH} | sed 's/boost-//')
     BOOST_TAR_FILE_NAME="${BOOST_BASE_NAME}.tar.gz"
     BOOST_DOWNLOAD_LINK="https://boostorg.jfrog.io/artifactory/main/release/${BOOST_BASE_VERSION}/source/${BOOST_TAR_FILE_NAME}"
-    travis_retry rm -rf ${BOOST_ROOT} && wget -q ${BOOST_DOWNLOAD_LINK}
+    rm -rf ${BOOST_ROOT}
+    travis_retry wget -q ${BOOST_DOWNLOAD_LINK}
     tar -xf "$BOOST_TAR_FILE_NAME"
     rm "$BOOST_TAR_FILE_NAME"
 
