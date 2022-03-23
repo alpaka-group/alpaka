903f143f8a242671075832de60e331584fe9be2e
Jan Stephan <j.stephan@hzdr.de>
2022-03-23 17:13:07 +0100

    Fix typos

--- include/alpaka/core/CudaHipMath.hpp
+++ include/alpaka/core/CudaHipMath.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2022 Benjamin Worpitz, René Widera, Sergei Bastrakov, Andrea Bocci
+/* Copyright 2022 Benjamin Worpitz, René Widera, Sergei Bastrakov, Andrea Bocci, Jan Stephan
  *
  * This file is part of alpaka.
  *
@@ -15,6 +15,6 @@
 #    include <cuda_runtime.h>
 #endif
 
-#if defined(ALPAKA_ACC_GPU_HIP_ENABLED) && defined(__HIP_EVICE_COMPILE__)
+#if defined(ALPAKA_ACC_GPU_HIP_ENABLED) && defined(__HIP_DEVICE_COMPILE__)
 #    include <hip/math_functions.h>
 #endif
