afbbeee71445caa52d53650910bff08948477eb3
Andrea Bocci <andrea.bocci@cern.ch>
2022-09-30 07:00:51 +0200

    Add elementwise_min and max functions
    Signed-off-by: Andrea Bocci <andrea.bocci@cern.ch>

--- include/alpaka/vec/Vec.hpp
+++ include/alpaka/vec/Vec.hpp
@@ -454,6 +454,42 @@ namespace alpaka
     template<typename TFirstIndex, typename... TRestIndices>
     Vec(TFirstIndex&&, TRestIndices&&...) -> Vec<DimInt<1 + sizeof...(TRestIndices)>, std::decay_t<TFirstIndex>>;
 
+    //! \return The element-wise minimum of one or more vectors.
+    ALPAKA_NO_HOST_ACC_WARNING
+    template<
+        typename TDim,
+        typename TVal,
+        typename... Vecs,
+        typename = std::enable_if_t<(std::is_same_v<Vec<TDim, TVal>, Vecs> && ...)>>
+    ALPAKA_FN_HOST_ACC constexpr auto elementwise_min(Vec<TDim, TVal> const& p, Vecs const&... qs) -> Vec<TDim, TVal>
+    {
+        Vec<TDim, TVal> r;
+        if constexpr(TDim::value > 0)
+        {
+            for(typename TDim::value_type i = 0; i < TDim::value; ++i)
+                r[i] = std::min({p[i], qs[i]...});
+        }
+        return r;
+    }
+
+    //! \return The element-wise maximum of one or more vectors.
+    ALPAKA_NO_HOST_ACC_WARNING
+    template<
+        typename TDim,
+        typename TVal,
+        typename... Vecs,
+        typename = std::enable_if_t<(std::is_same_v<Vec<TDim, TVal>, Vecs> && ...)>>
+    ALPAKA_FN_HOST_ACC constexpr auto elementwise_max(Vec<TDim, TVal> const& p, Vecs const&... qs) -> Vec<TDim, TVal>
+    {
+        Vec<TDim, TVal> r;
+        if constexpr(TDim::value > 0)
+        {
+            for(typename TDim::value_type i = 0; i < TDim::value; ++i)
+                r[i] = std::max({p[i], qs[i]...});
+        }
+        return r;
+    }
+
     namespace trait
     {
         //! The Vec dimension get trait specialization.
