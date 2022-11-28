da13776411946ba0f69d3ccba96cd161293cbe83
Luca <ferragina.luca@gmail.com>
2022-11-28 12:22:22 +0100

    Mark mysqrt as ALPAKA_FN_EXTERN

--- test/integ/separableCompilation/include/mysqrt.hpp
+++ test/integ/separableCompilation/include/mysqrt.hpp
@@ -1,7 +1,7 @@
-/* Copyright 2019 Benjamin Worpitz
+/* Copyright 2022 Benjamin Worpitz, Luca Ferragina
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #include <alpaka/alpaka.hpp>
 
-ALPAKA_FN_HOST_ACC auto mysqrt(double x) -> double;
+ALPAKA_FN_HOST_ACC ALPAKA_FN_EXTERN auto mysqrt(double x) -> double;
