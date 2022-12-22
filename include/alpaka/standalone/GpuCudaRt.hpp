4e6fecb3d735f36896c2eddc9a54c68660f8e43f
Jan Stephan <j.stephan@hzdr.de>
2022-12-22 15:49:16 +0100

    CUDA CI update

--- include/alpaka/standalone/GpuCudaRt.hpp
+++ include/alpaka/standalone/GpuCudaRt.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2019 Benjamin Worpitz
+/* Copyright 2022 Benjamin Worpitz, Jan Stephan
  *
  * This file is part of alpaka.
  *
@@ -11,4 +11,16 @@
 
 #ifndef ALPAKA_ACC_GPU_CUDA_ENABLED
 #    define ALPAKA_ACC_GPU_CUDA_ENABLED
+#endif
+
+#include <alpaka/core/BoostPredef.hpp>
+
+#if defined(BOOST_COMP_CLANG_CUDA) && (BOOST_COMP_CLANG_CUDA == BOOST_VERSION_NUMBER(14, 0, 0))
+
+#    include <cuda.h>
+
+#    if(CUDART_VERSION == 11030)
+#        error "clang-14 cannot be used as CUDA compiler when using CUDA v11.3. See alpaka GitHub issue 1857."
+#    endif
+
 #endif
