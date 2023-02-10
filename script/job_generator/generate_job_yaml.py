0567915e33ef8ff13c2db90cd198422ed6020902
Jan Stephan <j.stephan@hzdr.de>
2023-02-10 11:49:40 +0100

    Add ROCm 5.4 support
    Co-authored-by: Simeon Ehrig <s.ehrig@hzdr.de>

--- script/job_generator/generate_job_yaml.py
+++ script/job_generator/generate_job_yaml.py
@@ -191,6 +191,8 @@ def job_variables(job: Dict[str, Tuple[str, str]]) -> Dict[str, str]:
             variables["ALPAKA_CI_CLANG_VER"] = "14"
         elif job[DEVICE_COMPILER][VERSION] == "5.3":
             variables["ALPAKA_CI_CLANG_VER"] = "15"
+        elif job[DEVICE_COMPILER][VERSION] == "5.4":
+            variables["ALPAKA_CI_CLANG_VER"] = "15"
         else:
             raise RuntimeError(
                 "generate_job_yaml.job_variables(): unknown hip version: "
