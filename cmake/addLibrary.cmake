759d7545770e0a4d0f3cdf6778acd90dd705a267
Tools <alpaka@hzdr.de>
2023-02-13 01:30:56 +0100

    Use SPDX License identifiers

--- cmake/addLibrary.cmake
+++ cmake/addLibrary.cmake
@@ -1,11 +1,6 @@
 #
 # Copyright 2015-2021 Benjamin Worpitz, Maximilian Knespel, Simeon Ehrig
-#
-# This file is part of alpaka.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
 #
 
 CMAKE_MINIMUM_REQUIRED(VERSION 3.18)
--- test/integ/zeroDimBuffer/src/zeroDimBuffer.cpp
+++ test/integ/zeroDimBuffer/src/zeroDimBuffer.cpp
@@ -1,10 +1,5 @@
 /* Copyright 2022 Andrea Bocci, Jan Stephan
- *
- * This file is part of alpaka.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 // make sure the CPU_B_SEQ_T_SEQ backend is always available
