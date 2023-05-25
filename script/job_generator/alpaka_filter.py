0b25fc9e7e2c0d3621bf1356dd6382a0c8f6ff46
Simeon Ehrig <s.ehrig@hzdr.de>
2023-05-25 16:31:01 +0200

    GitLab CI: enable nvcc 12.x c++20 test
    - The alpaka-job-coverage library disabled accidentally C++ 20 tests for nvcc 12.x. ajc 1.3.1 solves the bug.
    - Removed some filter rules, which are integrated in ajc 1.3.1 now.
    - applied black format on generate_job_yaml.py (forgot in PR #1951)

--- script/job_generator/alpaka_filter.py
+++ script/job_generator/alpaka_filter.py
@@ -9,34 +9,6 @@ from typing import List
 from alpaka_job_coverage.globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
 from alpaka_globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
-from alpaka_job_coverage.util import (
-    row_check_name,
-    row_check_version,
-    row_check_backend_version,
-)
-
 
 def alpaka_post_filter(row: List) -> bool:
-    # disable clang as host compiler for nvcc 11.3 until 11.5
-    # https://github.com/alpaka-group/alpaka/issues/1625
-    if row_check_name(row, DEVICE_COMPILER, "==", NVCC) and row_check_name(
-        row, HOST_COMPILER, "==", CLANG
-    ):
-        if row_check_version(row, DEVICE_COMPILER, ">=", "11.3") and row_check_version(
-            row, DEVICE_COMPILER, "<=", "11.5"
-        ):
-            return False
-
-    # disable all clang versions older than 14 as CUDA Compiler
-    # https://github.com/alpaka-group/alpaka/issues/1857
-    if row_check_backend_version(
-        row, ALPAKA_ACC_GPU_CUDA_ENABLE, "!=",OFF_VER
-    ) and row_check_name(row, DEVICE_COMPILER, "==", CLANG_CUDA):
-        if row_check_version(row, DEVICE_COMPILER, "<", "14"):
-            return False
-
-        # a bug in CMAKE 3.18 avoids the correct usage of the variable CMAKE_CUDA_ARCHITECTURE
-        if row_check_version(row, CMAKE, "<", "3.19"):
-            return False
-
     return True
