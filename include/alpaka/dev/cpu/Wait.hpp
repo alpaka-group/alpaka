19bbc91689123b7a9651217381418fe88a8c96ac
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-06-14 17:05:40 +0200

    Refactoring

--- include/alpaka/dev/cpu/Wait.hpp
+++ include/alpaka/dev/cpu/Wait.hpp
@@ -8,23 +8,20 @@
 #include "alpaka/event/EventCpu.hpp"
 #include "alpaka/wait/Traits.hpp"
 
-namespace alpaka
+namespace alpaka::trait
 {
-    namespace trait
+    //! The CPU device thread wait specialization.
+    //!
+    //! Blocks until the device has completed all preceding requested tasks.
+    //! Tasks that are enqueued or queues that are created after this call is made are not waited for.
+    template<>
+    struct CurrentThreadWaitFor<DevCpu>
     {
-        //! The CPU device thread wait specialization.
-        //!
-        //! Blocks until the device has completed all preceding requested tasks.
-        //! Tasks that are enqueued or queues that are created after this call is made are not waited for.
-        template<>
-        struct CurrentThreadWaitFor<DevCpu>
+        ALPAKA_FN_HOST static auto currentThreadWaitFor(DevCpu const& dev) -> void
         {
-            ALPAKA_FN_HOST static auto currentThreadWaitFor(DevCpu const& dev) -> void
-            {
-                ALPAKA_DEBUG_FULL_LOG_SCOPE;
+            ALPAKA_DEBUG_FULL_LOG_SCOPE;
 
-                generic::currentThreadWaitForDevice(dev);
-            }
-        };
-    } // namespace trait
-} // namespace alpaka
+            generic::currentThreadWaitForDevice(dev);
+        }
+    };
+} // namespace alpaka::trait
