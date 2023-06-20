d6eb7146d4077872c7ac4ac50b493e9b86abbc8d
Jan Stephan <j.stephan@hzdr.de>
2023-06-20 14:30:58 +0200

    Add gcc-13 test runner

--- script/job_generator/versions.py
+++ script/job_generator/versions.py
@@ -12,7 +12,7 @@ from alpaka_globals import *  # pylint: disable=wildcard-import,unused-wildcard-
 
 # TODO: only an example
 sw_versions: Dict[str, List[str]] = {
-    GCC: ["9", "10", "11", "12"],
+    GCC: ["9", "10", "11", "12", "13"],
     CLANG: ["9", "10", "11", "12", "13", "14", "15"],
     NVCC: [
         "11.0",
