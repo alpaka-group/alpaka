2d0351fd4eb53f9cd6c63ba057953e8545ef94fd
GitHub Action <action@github.com>
2023-06-24 12:26:43 +0000

    Update alpaka.hpp

--- include/alpaka/alpaka.hpp
+++ include/alpaka/alpaka.hpp
@@ -34125,6 +34125,39 @@
 	// ============================================================================
 
 // #include "alpaka/meta/NdLoop.hpp"    // amalgamate: file already expanded
+	// ============================================================================
+	// == ./include/alpaka/meta/NonZero.hpp ==
+	// ==
+	/* Copyright 2023 Andrea Bocci
+	 * SPDX-License-Identifier: MPL-2.0
+	 */
+
+	// #pragma once
+	// #include <type_traits>    // amalgamate: file already included
+
+	namespace alpaka::meta
+	{
+	    namespace detail
+	    {
+	        template<typename T>
+	        struct NonZeroImpl : std::false_type
+	        {
+	        };
+
+	        template<typename T, T TValue>
+	        struct NonZeroImpl<std::integral_constant<T, TValue>> : std::bool_constant<TValue != static_cast<T>(0)>
+	        {
+	        };
+	    } // namespace detail
+
+	    template<typename T>
+	    using NonZero = typename detail::NonZeroImpl<T>;
+
+	} // namespace alpaka::meta
+	// ==
+	// == ./include/alpaka/meta/NonZero.hpp ==
+	// ============================================================================
+
 // #include "alpaka/meta/Set.hpp"    // amalgamate: file already expanded
 	// ============================================================================
 	// == ./include/alpaka/meta/Transform.hpp ==
