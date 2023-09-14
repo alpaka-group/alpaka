7a7d9c7c95428062f9dcb27d0f1d9ea101d90e40
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-09-14 18:22:47 +0200

    Workaround gcc warning on uninitialized PlatformCpu

--- include/alpaka/test/queue/QueueTestFixture.hpp
+++ include/alpaka/test/queue/QueueTestFixture.hpp
@@ -12,12 +12,6 @@ namespace alpaka::test
     template<typename TDevQueue>
     struct QueueTestFixture
     {
-#if defined(BOOST_COMP_GNUC) && BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(11, 0, 0)                                     \
-    && BOOST_COMP_GNUC < BOOST_VERSION_NUMBER(12, 0, 0)
-// g++-11 (wrongly) believes that m_platform is used in an uninitialized state.
-#    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#endif
         using Dev = std::tuple_element_t<0, TDevQueue>;
         using Queue = std::tuple_element_t<1, TDevQueue>;
         using Platform = alpaka::Platform<Dev>;
@@ -25,9 +19,5 @@ namespace alpaka::test
         Platform m_platform{};
         Dev m_dev{getDevByIdx(m_platform, 0)};
         Queue m_queue{m_dev};
-#if defined(BOOST_COMP_GNUC) && BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(11, 0, 0)                                     \
-    && BOOST_COMP_GNUC < BOOST_VERSION_NUMBER(12, 0, 0)
-#    pragma GCC diagnostic pop
-#endif
     };
 } // namespace alpaka::test
