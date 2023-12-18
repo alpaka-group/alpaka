d30a12910e1f08a62405c3fdc4ee784ca6765b94
Simeon Ehrig <s.ehrig@hzdr.de>
2023-12-18 14:33:23 +0100

    add ROCm CI for version 5.6, 5.7

--- script/job_generator/generate_job_yaml.py
+++ script/job_generator/generate_job_yaml.py
@@ -349,6 +349,10 @@ def job_variables(job: Dict[str, Tuple[str, str]]) -> Dict[str, str]:
             variables["ALPAKA_CI_CLANG_VER"] = "15"
         elif job[DEVICE_COMPILER][VERSION] == "5.5":
             variables["ALPAKA_CI_CLANG_VER"] = "16"
+        elif job[DEVICE_COMPILER][VERSION] == "5.6":
+            variables["ALPAKA_CI_CLANG_VER"] = "16"
+        elif job[DEVICE_COMPILER][VERSION] == "5.7":
+            variables["ALPAKA_CI_CLANG_VER"] = "17"
         else:
             raise RuntimeError(
                 "generate_job_yaml.job_variables(): unknown hip version: "
