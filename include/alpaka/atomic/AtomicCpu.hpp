73642da70d35d0936b57cceddda108055ce86c87
Andrea Bocci <andrea.bocci@cern.ch>
2024-06-04 19:11:36 +0200

    Enable AtomicAtomicRef when possible

--- include/alpaka/atomic/AtomicCpu.hpp
+++ include/alpaka/atomic/AtomicCpu.hpp
@@ -1,14 +1,19 @@
-/* Copyright 2021 Andrea Bocci, Felice Pantaleo
+/* Copyright 2024 Andrea Bocci, Felice Pantaleo
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include <boost/version.hpp>
+#include "alpaka/core/BoostPredef.hpp"
 
-#ifndef ALPAKA_DISABLE_ATOMIC_ATOMICREF
-#    define ALPAKA_DISABLE_ATOMIC_ATOMICREF
-#endif
+// clang 9 fails at compile time when using boost::atomic_ref
+#ifdef BOOST_COMP_CLANG_AVAILABLE
+#    if BOOST_COMP_CLANG < BOOST_VERSION_NUMBER(11, 0, 0)
+#        ifndef ALPAKA_DISABLE_ATOMIC_ATOMICREF
+#            define ALPAKA_DISABLE_ATOMIC_ATOMICREF
+#        endif
+#    endif
+#endif // BOOST_COMP_CLANG_AVAILABLE
 
 #include "alpaka/atomic/AtomicAtomicRef.hpp"
 #include "alpaka/atomic/AtomicStdLibLock.hpp"
