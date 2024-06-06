34697da0096cd610da757b36b20e22ef739e32b5
mehmet yusufoglu <mehmetyusufoglu01@gmail.com>
2024-06-06 11:28:06 +0200

    CI_FILTER: ^linux_nvcc11.*clang

--- test/unit/workDiv/src/WorkDivHelpersTest.cpp
+++ test/unit/workDiv/src/WorkDivHelpersTest.cpp
@@ -12,6 +12,62 @@
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
+struct TestKernelWithManyRegisters
+{
+    template<typename TAcc>
+    [[maybe_unused]] ALPAKA_FN_ACC auto operator()(TAcc const&, std::size_t val) const -> void
+    {
+        double var0 = 1.0;
+        double var1 = 2.0;
+        double var2 = 3.0;
+
+        // Define many variables and use some calculations in order to prevent compiler optimization and make the
+        // kernel use many registers (around 80 on sm_52). Using many registers per SM decreases the max number of
+        // threads per block while this kernel is being run.
+
+        // TODO: Use function templates to parametrize and shorten the code!
+        double var3 = var2 + fmod(var2, 5);
+
+        double var4 = var3 + fmod(var3, 5);
+        double var5 = var4 + fmod(var4, 5);
+        double var6 = var5 + fmod(var5, 5);
+        double var7 = var6 + fmod(var6, 5);
+        double var8 = var7 + fmod(var7, 5);
+        double var9 = var8 + fmod(var8, 5);
+        double var10 = var9 + fmod(var9, 5);
+        double var11 = var10 + fmod(var10, 5);
+        double var12 = var11 + fmod(var11, 5);
+        double var13 = var12 + fmod(var12, 5);
+        double var14 = var13 + fmod(var13, 5);
+        double var15 = var14 + fmod(var14, 5);
+        double var16 = var15 + fmod(var15, 5);
+        double var17 = var16 + fmod(var16, 5);
+        double var18 = var17 + fmod(var17, 5);
+        double var19 = var18 + fmod(var18, 5);
+        double var20 = var19 + fmod(var19, 5);
+        double var21 = var20 + fmod(var20, 5);
+        double var22 = var21 + fmod(var21, 5);
+        double var23 = var22 + fmod(var22, 5);
+        double var24 = var23 + fmod(var23, 5);
+        double var25 = var24 + fmod(var24, 5);
+        double var26 = var25 + fmod(var25, 5);
+        double var27 = var26 + fmod(var26, 5);
+        double var28 = var27 + fmod(var27, 5);
+        double var29 = var28 + fmod(var28, 5);
+        double var30 = var29 + fmod(var29, 5);
+        double var31 = var30 + fmod(var30, 5);
+        double var32 = var31 + fmod(var31, 5);
+        double var33 = var32 + fmod(var32, 5);
+        double var34 = var33 + fmod(var33, 5);
+        double var35 = var34 + fmod(var34, 5);
+
+        double sum = var0 + var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var9 + var10 + var11 + var12
+                     + var13 + var14 + var15 + var16 + var17 + var18 + var19 + var20 + var21 + var22 + var23 + var24
+                     + var25 + var26 + var27 + var28 + var29 + var30 + var31 + var32 + var33 + var34 + var35;
+        printf("The sum is %5.2f, the argument is %lu ", sum, val);
+    }
+};
+
 namespace
 {
     template<typename TAcc>
@@ -240,9 +296,26 @@ TEMPLATE_LIST_TEST_CASE("testVectorCreation", "[workDiv]", TestAccs2D)
     // using Vec = alpaka::Vec<Dim, Idx>;
     using Dim = alpaka::Dim<Acc>;
     using Vec = alpaka::Vec<Dim, Idx>;
-
+    // using WorkDiv = alpaka::WorkDivMembers<Dim, Idx>;
+    auto const platform = alpaka::Platform<Acc>{};
+    auto const dev = alpaka::getDevByIdx(platform, 0);
 
     // Vec const gridThreadExtent(static_cast<Idx>(1), static_cast<Idx>(threadsPerGridTestValue / 8));
     Vec const gridThreadExtent(static_cast<Idx>(1), static_cast<Idx>(800 / 8));
     printf("%u", gridThreadExtent[0]);
+    auto blocksPerGrid3D = Vec{1u, 2u};
+
+
+    TestKernelWithManyRegisters kernel;
+
+
+    // Get hard limits for test
+    // auto const& props = alpaka::getAccDevProps<Acc>(dev);
+    // Idx const threadsPerGridTestValue = props.m_blockThreadCountMax * props.m_gridBlockCountMax;
+
+    // Test getValidWorkDivForKernel for threadsPerGridTestValue threads per grid
+    auto const workDiv = alpaka::getValidWorkDiv<Acc>(dev, gridThreadExtent, Vec{1, 1});
+    // Test validity
+    auto const isValid = alpaka::isValidWorkDiv<Acc>(dev, workDiv);
+    CHECK(isValid == true);
 }
