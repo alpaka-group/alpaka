a672c8afa890183365f74cce48167269c46a4104
Simeon Ehrig <s.ehrig@hzdr.de>
2024-07-17 08:09:56 +0200

    improve color echo
    CI_FILTER: ^linux_gcc13

--- script/setup_utilities.sh
+++ script/setup_utilities.sh
@@ -6,23 +6,30 @@
 
 : "${ALPAKA_CI_OS_NAME?'ALPAKA_CI_OS_NAME must be specified'}"
 
-# enable Terminal colors for MacOS
-if [ "$ALPAKA_CI_OS_NAME" = "macOS" ]; then
-    export CLICOLOR=1
-    export LSCOLORS=ExFxCxDxBxegedabagacad
-fi
-
 # display a message in green
 echo_green() {
-    echo -e "\e[1;32m$1\e[0m"
+    # macOS does not support bash colors
+    if [ "$ALPAKA_CI_OS_NAME" != "macOS" ]; then
+        echo -e "\e[1;32m$1\e[0m"
+    else
+        echo "$1"
+    if
 }
 
 # display a message in yellow
 echo_yellow() {
-    echo -e "\e[1;33m$1\e[0m"
+    if [ "$ALPAKA_CI_OS_NAME" != "macOS" ]; then
+        echo -e "\e[1;33m$1\e[0m"
+    else
+        echo "$1"
+    if
 }
 
 # display a message in red
 echo_red() {
-    echo -e "\e[1;31m$1\e[0m"
+    if [ "$ALPAKA_CI_OS_NAME" != "macOS" ]; then
+        echo -e "\e[1;31m$1\e[0m"
+    else
+        echo "$1"
+    if
 }
