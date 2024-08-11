513d8c007aae676bd1d8124ec5ec1e44db9b0127
AuroraPerego <aurora.perego@cern.ch>
2024-08-11 23:25:27 +0200

    remove code duplication in getFunctionAttributes for SYCL

--- include/alpaka/kernel/TaskKernelFpgaSyclIntel.hpp
+++ include/alpaka/kernel/TaskKernelFpgaSyclIntel.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "alpaka/kernel/TaskKernelGenericSycl.hpp"
+#include "alpaka/platform/PlatformFpgaSyclIntel.hpp"
 
 #if defined(ALPAKA_ACC_SYCL_ENABLED) && defined(ALPAKA_SYCL_ONEAPI_FPGA)
 
@@ -12,38 +13,8 @@ namespace alpaka
 {
     template<typename TDim, typename TIdx, typename TKernelFnObj, typename... TArgs>
     using TaskKernelFpgaSyclIntel
-        = TaskKernelGenericSycl<AccFpgaSyclIntel<TDim, TIdx>, TDim, TIdx, TKernelFnObj, TArgs...>;
+        = TaskKernelGenericSycl<detail::IntelFpgaSelector, AccFpgaSyclIntel<TDim, TIdx>, TDim, TIdx, TKernelFnObj, TArgs...>;
 
-    namespace trait
-    {
-        //! \brief Specialisation of the class template FunctionAttributes
-        //! \tparam TDev The device type.
-        //! \tparam TDim The dimensionality of the accelerator device properties.
-        //! \tparam TIdx The idx type of the accelerator device properties.
-        //! \tparam TKernelFn Kernel function object type.
-        //! \tparam TArgs Kernel function object argument types as a parameter pack.
-        template<typename TDev, typename TDim, typename TIdx, typename TKernelFn, typename... TArgs>
-        struct FunctionAttributes<AccFpgaSyclIntel<TDim, TIdx>, TDev, TKernelFn, TArgs...>
-        {
-            //! \param dev The device instance
-            //! \param kernelFn The kernel function object which should be executed.
-            //! \param args The kernel invocation arguments.
-            //! \return KernelFunctionAttributes instance. The default version always returns an instance with zero
-            //! fields. For CPU, the field of max threads allowed by kernel function for the block is 1.
-            ALPAKA_FN_HOST static auto getFunctionAttributes(
-                TDev const& dev,
-                [[maybe_unused]] TKernelFn const& kernelFn,
-                [[maybe_unused]] TArgs&&... args) -> alpaka::KernelFunctionAttributes
-            {
-                alpaka::KernelFunctionAttributes kernelFunctionAttributes;
-
-                // set function properties for maxThreadsPerBlock to device properties
-                auto const& props = alpaka::getAccDevProps<AccFpgaSyclIntel<TDim, TIdx>>(dev);
-                kernelFunctionAttributes.maxThreadsPerBlock = static_cast<int>(props.m_blockThreadCountMax);
-                return kernelFunctionAttributes;
-            }
-        };
-    } // namespace trait
 } // namespace alpaka
 
 #endif
