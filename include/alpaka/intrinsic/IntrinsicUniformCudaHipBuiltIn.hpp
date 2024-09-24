c30c8c1ef769cf35867557103f3848e57861f739
René Widera <r.widera@hzdr.de>
2024-09-24 10:32:30 +0200

    fix clang cuda 18+ `__popc` interface change
    With clang 18 the interface for `__popc` changed.

--- include/alpaka/intrinsic/IntrinsicUniformCudaHipBuiltIn.hpp
+++ include/alpaka/intrinsic/IntrinsicUniformCudaHipBuiltIn.hpp
@@ -36,7 +36,8 @@ namespace alpaka
             __device__ static auto popcount(IntrinsicUniformCudaHipBuiltIn const& /*intrinsic*/, std::uint32_t value)
                 -> std::int32_t
             {
-#        if BOOST_COMP_CLANG && BOOST_LANG_CUDA
+                // clang as CUDA compiler change the interface to unsigned values for clang >=18
+#        if BOOST_COMP_CLANG && BOOST_LANG_CUDA && BOOST_COMP_CLANG < BOOST_VERSION_NUMBER(18, 0, 0)
                 return __popc(static_cast<int>(value));
 #        else
                 return static_cast<std::int32_t>(__popc(static_cast<unsigned int>(value)));
@@ -46,7 +47,8 @@ namespace alpaka
             __device__ static auto popcount(IntrinsicUniformCudaHipBuiltIn const& /*intrinsic*/, std::uint64_t value)
                 -> std::int32_t
             {
-#        if BOOST_COMP_CLANG && BOOST_LANG_CUDA
+                // clang as CUDA compiler change the interface to unsigned values for clang >=18
+#        if BOOST_COMP_CLANG && BOOST_LANG_CUDA && BOOST_COMP_CLANG < BOOST_VERSION_NUMBER(18, 0, 0)
                 return __popcll(static_cast<long long>(value));
 #        else
                 return static_cast<std::int32_t>(__popcll(static_cast<unsigned long long>(value)));
