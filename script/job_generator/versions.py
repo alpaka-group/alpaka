1e24b5afdbf36c304295018074295c4151eb93ce
Andrea Bocci <andrea.bocci@cern.ch>
2025-01-26 19:38:14 +0100

    Update to oneAPI 2025.0

--- script/job_generator/versions.py
+++ script/job_generator/versions.py
@@ -30,7 +30,7 @@ sw_versions: Dict[str, List[str]] = {
         "12.6",
     ],
     HIPCC: ["5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "6.0", "6.1", "6.2"],
-    ICPX: ["2024.2"],
+    ICPX: ["2025.0"],
     # Contains all enabled back-ends.
     # There are special cases for ALPAKA_ACC_GPU_CUDA_ENABLE and ALPAKA_ACC_GPU_HIP_ENABLE
     # which have to be combined with nvcc and hipcc versions.
