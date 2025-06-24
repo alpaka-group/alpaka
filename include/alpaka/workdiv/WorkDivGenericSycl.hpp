fb0edf27422baa728cd4903e452d2ac37d1102fc
Andrea Bocci <andrea.bocci@cern.ch>
2025-06-24 18:50:31 +0200

    SYCL: fix index order (#2526)
    The fast moving index in SYCL's nd_item is the rightmost one, equal to
    alpaka's index order.
    In our code base we implemented it equal to CUDA's index order where the
    left most index is the fast moving index.
    
    Co-authored-by: René Widera <r.widera@hzdr.de>

--- include/alpaka/workdiv/WorkDivGenericSycl.hpp
+++ include/alpaka/workdiv/WorkDivGenericSycl.hpp
@@ -64,15 +64,15 @@ namespace alpaka::trait
             else if constexpr(TDim::value == 2)
             {
                 return Vec<TDim, TIdx>{
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(1)),
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(0))};
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(0)),
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(1))};
             }
             else
             {
                 return Vec<TDim, TIdx>{
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(2)),
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(0)),
                     static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(1)),
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(0))};
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_group_range(2))};
             }
         }
     };
@@ -91,15 +91,15 @@ namespace alpaka::trait
             else if constexpr(TDim::value == 2)
             {
                 return Vec<TDim, TIdx>{
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(1)),
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(0))};
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(0)),
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(1))};
             }
             else
             {
                 return Vec<TDim, TIdx>{
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(2)),
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(0)),
                     static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(1)),
-                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(0))};
+                    static_cast<TIdx>(workDiv.m_item_workdiv.get_local_range(2))};
             }
         }
     };
