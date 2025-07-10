521e0d18be5e4b77665e5f356f01c24ce48f410b
GitHub Action <action@github.com>
2025-07-10 07:54:50 +0000

    Update alpaka.hpp

--- include/alpaka/alpaka.hpp
+++ include/alpaka/alpaka.hpp
@@ -10493,6 +10493,128 @@
 		 */
 
 		// #pragma once
+			// ============================================================================
+			// == ./include/alpaka/acc/Tag.hpp ==
+			// ==
+			/* Copyright 2023 Simeon Ehrig, Jan Stephan, Andrea Bocci
+			 * SPDX-License-Identifier: MPL-2.0
+			 */
+
+			// #pragma once
+			// #include "alpaka/core/Config.hpp"    // amalgamate: file already inlined
+
+			// #include <iostream>    // amalgamate: file already included
+			// #include <tuple>    // amalgamate: file already included
+			#include <type_traits>
+
+			namespace alpaka
+			{
+			    struct InterfaceTag
+			    {
+			    };
+			} // namespace alpaka
+
+			#define CREATE_ACC_TAG(tag_name)                                                                                      \
+			    struct tag_name : public alpaka::InterfaceTag                                                                     \
+			    {                                                                                                                 \
+			        static std::string get_name()                                                                                 \
+			        {                                                                                                             \
+			            return #tag_name;                                                                                         \
+			        }                                                                                                             \
+			    }
+
+			namespace alpaka
+			{
+			    CREATE_ACC_TAG(TagCpuOmp2Blocks);
+			    CREATE_ACC_TAG(TagCpuOmp2Threads);
+			    CREATE_ACC_TAG(TagCpuSerial);
+			    CREATE_ACC_TAG(TagCpuSycl);
+			    CREATE_ACC_TAG(TagCpuTbbBlocks);
+			    CREATE_ACC_TAG(TagCpuThreads);
+			    CREATE_ACC_TAG(TagFpgaSyclIntel);
+			    CREATE_ACC_TAG(TagGenericSycl);
+			    CREATE_ACC_TAG(TagGpuCudaRt);
+			    CREATE_ACC_TAG(TagGpuHipRt);
+			    CREATE_ACC_TAG(TagGpuSyclIntel);
+
+			    namespace concepts
+			    {
+			        template<typename T>
+			        concept Tag = requires {
+			            {
+			                T::get_name()
+			            } -> std::same_as<std::string>;
+			            requires std::default_initializable<T>;
+			            requires std::derived_from<T, alpaka::InterfaceTag>;
+			        };
+			    } // namespace concepts
+
+			    namespace trait
+			    {
+			        template<typename TAcc>
+			        struct AccToTag;
+
+			        template<concepts::Tag TTag, typename TDim, typename TIdx>
+			        struct TagToAcc;
+			    } // namespace trait
+
+			    //! \brief maps an acc type to a tag type
+			    //! \tparam TAcc alpaka acc type
+			    template<typename TAcc>
+			    using AccToTag = typename trait::AccToTag<TAcc>::type;
+
+			    //! \brief maps a tag type to an acc type
+			    //! \tparam TTag alpaka tag type
+			    //! \tparam TDim dimension of the mapped acc type
+			    //! \tparam TIdx index type of the mapped acc type
+			    template<concepts::Tag TTag, typename TDim, typename TIdx>
+			    using TagToAcc = typename trait::TagToAcc<TTag, TDim, TIdx>::type;
+
+			    template<typename TAcc, concepts::Tag... TTag>
+			    inline constexpr bool accMatchesTags = (std::is_same_v<alpaka::AccToTag<TAcc>, TTag> || ...);
+
+			    //! list of all available tags
+			    using AccTags = std::tuple<
+			        alpaka::TagCpuSerial,
+			        alpaka::TagCpuThreads,
+			        alpaka::TagCpuTbbBlocks,
+			        alpaka::TagCpuOmp2Blocks,
+			        alpaka::TagCpuOmp2Threads,
+			        alpaka::TagGpuCudaRt,
+			        alpaka::TagGpuHipRt,
+			        alpaka::TagCpuSycl,
+			        alpaka::TagFpgaSyclIntel,
+			        alpaka::TagGpuSyclIntel>;
+
+			    //!  \brief Function to print the names of each tag in the given tuple of tags
+			    //!  \tparam TTuple is the type of the tuple of tags
+			    template<typename TTuple>
+			    void printTagNames()
+			    {
+			        // Check if the tuple is empty using std::tuple_size_v
+			        if(std::tuple_size_v<TTuple> == 0)
+			        {
+			            std::cout << "No Tags!";
+			        }
+			        else
+			        {
+			            std::cout << "Tags: ";
+			            // Print tags with comma in between
+			            std::apply(
+			                [](auto... args)
+			                {
+			                    auto index = std::tuple_size_v<TTuple>;
+			                    ((std::cout << args.get_name() << (--index > 0u ? "," : "")), ...);
+			                },
+			                TTuple{});
+			        }
+			        std::cout << std::endl;
+			    }
+			} // namespace alpaka
+			// ==
+			// == ./include/alpaka/acc/Tag.hpp ==
+			// ============================================================================
+
 		// #include "alpaka/acc/Traits.hpp"    // amalgamate: file already inlined
 		// #include "alpaka/core/Common.hpp"    // amalgamate: file already inlined
 		// #include "alpaka/core/Config.hpp"    // amalgamate: file already inlined
@@ -10992,6 +11114,34 @@
 		    {
 		        enqueue(queue, createTaskKernel<TAcc>(workDiv, kernelFnObj, std::forward<TArgs>(args)...));
 		    }
+
+		#if ALPAKA_COMP_CLANG
+		#    pragma clang diagnostic push
+		#    pragma clang diagnostic ignored                                                                                  \
+		        "-Wdocumentation" // clang does not support the syntax for variadic template arguments "args,..."
+		#endif
+		//! Executes the given kernel in the given queue.
+		//!
+		//! \tparam TTag The tag type.
+		//! \param queue The queue to enqueue the view copy task into.
+		//! \param workDiv The index domain work division.
+		//! \param kernelFnObj The kernel function object which should be executed.
+		//! \param args,... The kernel invocation arguments.
+		#if ALPAKA_COMP_CLANG
+		#    pragma clang diagnostic pop
+		#endif
+		    template<concepts::Tag TTag, typename TQueue, typename TWorkDiv, typename TKernelFnObj, typename... TArgs>
+		    ALPAKA_FN_HOST auto exec(TQueue& queue, TWorkDiv const& workDiv, TKernelFnObj const& kernelFnObj, TArgs&&... args)
+		        -> void
+		    {
+		        enqueue(
+		            queue,
+		            createTaskKernel<TagToAcc<TTag, Dim<std::decay_t<TWorkDiv>>, Idx<std::decay_t<TWorkDiv>>>>(
+		                workDiv,
+		                kernelFnObj,
+		                std::forward<TArgs>(args)...));
+		    }
+
 		} // namespace alpaka
 		// ==
 		// == ./include/alpaka/kernel/Traits.hpp ==
@@ -11000,128 +11150,7 @@
 	// #include "alpaka/platform/Traits.hpp"    // amalgamate: file already inlined
 
 	// Implementation details.
-		// ============================================================================
-		// == ./include/alpaka/acc/Tag.hpp ==
-		// ==
-		/* Copyright 2023 Simeon Ehrig, Jan Stephan, Andrea Bocci
-		 * SPDX-License-Identifier: MPL-2.0
-		 */
-
-		// #pragma once
-		// #include "alpaka/core/Config.hpp"    // amalgamate: file already inlined
-
-		// #include <iostream>    // amalgamate: file already included
-		// #include <tuple>    // amalgamate: file already included
-		#include <type_traits>
-
-		namespace alpaka
-		{
-		    struct InterfaceTag
-		    {
-		    };
-		} // namespace alpaka
-
-		#define CREATE_ACC_TAG(tag_name)                                                                                      \
-		    struct tag_name : public alpaka::InterfaceTag                                                                     \
-		    {                                                                                                                 \
-		        static std::string get_name()                                                                                 \
-		        {                                                                                                             \
-		            return #tag_name;                                                                                         \
-		        }                                                                                                             \
-		    }
-
-		namespace alpaka
-		{
-		    CREATE_ACC_TAG(TagCpuOmp2Blocks);
-		    CREATE_ACC_TAG(TagCpuOmp2Threads);
-		    CREATE_ACC_TAG(TagCpuSerial);
-		    CREATE_ACC_TAG(TagCpuSycl);
-		    CREATE_ACC_TAG(TagCpuTbbBlocks);
-		    CREATE_ACC_TAG(TagCpuThreads);
-		    CREATE_ACC_TAG(TagFpgaSyclIntel);
-		    CREATE_ACC_TAG(TagGenericSycl);
-		    CREATE_ACC_TAG(TagGpuCudaRt);
-		    CREATE_ACC_TAG(TagGpuHipRt);
-		    CREATE_ACC_TAG(TagGpuSyclIntel);
-
-		    namespace concepts
-		    {
-		        template<typename T>
-		        concept Tag = requires {
-		            {
-		                T::get_name()
-		            } -> std::same_as<std::string>;
-		            requires std::default_initializable<T>;
-		            requires std::derived_from<T, alpaka::InterfaceTag>;
-		        };
-		    } // namespace concepts
-
-		    namespace trait
-		    {
-		        template<typename TAcc>
-		        struct AccToTag;
-
-		        template<concepts::Tag TTag, typename TDim, typename TIdx>
-		        struct TagToAcc;
-		    } // namespace trait
-
-		    //! \brief maps an acc type to a tag type
-		    //! \tparam TAcc alpaka acc type
-		    template<typename TAcc>
-		    using AccToTag = typename trait::AccToTag<TAcc>::type;
-
-		    //! \brief maps a tag type to an acc type
-		    //! \tparam TTag alpaka tag type
-		    //! \tparam TDim dimension of the mapped acc type
-		    //! \tparam TIdx index type of the mapped acc type
-		    template<concepts::Tag TTag, typename TDim, typename TIdx>
-		    using TagToAcc = typename trait::TagToAcc<TTag, TDim, TIdx>::type;
-
-		    template<typename TAcc, concepts::Tag... TTag>
-		    inline constexpr bool accMatchesTags = (std::is_same_v<alpaka::AccToTag<TAcc>, TTag> || ...);
-
-		    //! list of all available tags
-		    using AccTags = std::tuple<
-		        alpaka::TagCpuSerial,
-		        alpaka::TagCpuThreads,
-		        alpaka::TagCpuTbbBlocks,
-		        alpaka::TagCpuOmp2Blocks,
-		        alpaka::TagCpuOmp2Threads,
-		        alpaka::TagGpuCudaRt,
-		        alpaka::TagGpuHipRt,
-		        alpaka::TagCpuSycl,
-		        alpaka::TagFpgaSyclIntel,
-		        alpaka::TagGpuSyclIntel>;
-
-		    //!  \brief Function to print the names of each tag in the given tuple of tags
-		    //!  \tparam TTuple is the type of the tuple of tags
-		    template<typename TTuple>
-		    void printTagNames()
-		    {
-		        // Check if the tuple is empty using std::tuple_size_v
-		        if(std::tuple_size_v<TTuple> == 0)
-		        {
-		            std::cout << "No Tags!";
-		        }
-		        else
-		        {
-		            std::cout << "Tags: ";
-		            // Print tags with comma in between
-		            std::apply(
-		                [](auto... args)
-		                {
-		                    auto index = std::tuple_size_v<TTuple>;
-		                    ((std::cout << args.get_name() << (--index > 0u ? "," : "")), ...);
-		                },
-		                TTuple{});
-		        }
-		        std::cout << std::endl;
-		    }
-		} // namespace alpaka
-		// ==
-		// == ./include/alpaka/acc/Tag.hpp ==
-		// ============================================================================
-
+	// #include "alpaka/acc/Tag.hpp"    // amalgamate: file already inlined
 		// ============================================================================
 		// == ./include/alpaka/core/ClipCast.hpp ==
 		// ==
