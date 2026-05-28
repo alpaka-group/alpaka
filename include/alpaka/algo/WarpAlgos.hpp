c10ad652b36af3215cb6ae0c759dbcca99f503d6
alexstrel <astrel@fnal.gov>
2026-05-27 19:51:40 -0500

    improved warp algos helper functions

--- include/alpaka/algo/WarpAlgos.hpp
+++ include/alpaka/algo/WarpAlgos.hpp
@@ -33,7 +33,7 @@ namespace alpaka
  * 
  * @return compute lane mask:
  */
- template<typename TAcc>
+ template<concepts::Acc TAcc>
  ALPAKA_FN_ACC constexpr inline auto get_lane_mask(std::uint32_t const lane_idx) -> typename TAcc::mask_type {
     #if defined(__HIP_DEVICE_COMPILE__) && defined(ALPAKA_ACC_GPU_HIP_ENABLED)
         return (1ULL << lane_idx);
@@ -50,7 +50,7 @@ namespace alpaka
  * 
  * @return True if active, otherwise false.
  */
- template<typename TAcc>
+ template<concepts::Acc TAcc>
  ALPAKA_FN_ACC constexpr inline bool is_work_lane(typename TAcc::mask_type const work_mask, std::uint32_t const lane_idx) {
     return ((work_mask >> lane_idx) & 1);
   }
@@ -65,18 +65,20 @@ namespace alpaka
  * 
  * @return Index of least significant 1 bit (0-based). (or warp size if x == 0).
  */
- template <alpaka::concepts::Acc TAcc>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE warp::warp_mask_t get_ls1b_idx(TAcc const& acc, const warp::warp_mask_t mask) {
-   if (mask == 0)
-     return static_cast<warp::warp_mask_t>(get_warp_size<TAcc>());
+ template <concepts::Acc TAcc>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE auto get_ls1b_idx(TAcc const& acc, typename TAcc::mask_type const mask) -> typename TAcc::mask_type {
+   using mask_t = typename TAcc::mask_type;
+
+   if (mask == 0) 
+     return static_cast<mask_t>(warp::getSizeCompileTime<TAcc>());
 
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return 0;
 
-   using signed_warp_mask_t = std::make_signed_t<warp::warp_mask_t>;
+   using signed_warp_mask_t = std::make_signed_t<mask_t>;
 
    const auto pos = alpaka::ffs(acc, static_cast<signed_warp_mask_t>(mask));
-   return static_cast<warp::warp_mask_t>(pos - 1);
+   return static_cast<mask_t>(pos - 1);
  }
 
   /**
@@ -90,15 +92,17 @@ namespace alpaka
  * 
  * @return True if lane_idx is the least significant bit in a mask, otherwise faulse .
  */
- template <alpaka::concepts::Acc TAcc>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE bool is_ls1b_idx(const warp::warp_mask_t mask, const unsigned int lane_idx) {
+ template <concepts::Acc TAcc>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE bool is_ls1b_idx(typename TAcc::mask_type const mask, uint32_t const lane_idx) {
+   using mask_t = typename TAcc::mask_type;
+
    if (mask == 0)
      return false;
 
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return true;
 
-   const warp::warp_mask_t lane_mask = get_lane_mask(lane_idx);
+   const mask_t lane_mask = get_lane_mask<TAcc>(lane_idx);
 
    // First check wether the lane is represented at all, otherwise check the trivial case:
    if ((mask & lane_mask) == 0)
@@ -106,9 +110,9 @@ namespace alpaka
    else if (lane_idx == 0)
      return true;
 
-   constexpr unsigned int w_extent = get_warp_size<TAcc>();
+   constexpr std::uint32_t w_extent = warp::getSizeCompileTime<TAcc>();
 
-   const warp::warp_mask_t inverted_mask = mask << (w_extent - lane_idx);
+   const mask_t inverted_mask = mask << (w_extent - lane_idx);
 
    return (inverted_mask == 0);
  }
@@ -129,35 +133,37 @@ namespace alpaka
 * - lane 0 receives the total sum over the warp (used as the per-warp NNZ aggregate)
 */
 
- template <alpaka::concepts::Acc TAcc, bool all = true>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE unsigned int warp_exclusive_sum(TAcc const& acc,
-                                                                unsigned int val,
-                                                                const unsigned int lane_idx) {
+ template <concepts::Acc TAcc, bool all = true>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE std::uint32_t warp_exclusive_sum(TAcc const& acc,
+                                                                std::uint32_t const val,
+                                                                std::uint32_t const lane_idx) {
+   using mask_t = typename TAcc::mask_type;
+
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return all ? val : 0;
 
-   const auto full_mask = alpaka::warp::ballot(acc, true);
+   const mask_t full_mask = warp::ballot(acc, true);
 
-   constexpr unsigned int w_extent = get_warp_size<TAcc>();
+   constexpr std::uint32_t w_extent = warp::getSizeCompileTime<TAcc>();
 
-   unsigned int local_offset = val;
+   const std::uint32_t local_offset = val;
 
    // Do inclusive sum first:
    //CMS_UNROLL_LOOP
-   for (unsigned int step = 1; step < w_extent; step *= 2) {
-     const auto res = alpaka::warp::shfl_up(acc, local_offset, step, w_extent);
+   for (std::uint32_t step = 1; step < w_extent; step *= 2) {
+     const auto res = warp::shfl_up(acc, local_offset, step, w_extent);
      if (lane_idx >= step)
        local_offset += res;
    }
 
-   warp::syncWarpThreads_mask(acc, full_mask);
+   warp::syncWarpThreads(acc, full_mask);
 
    if constexpr (all) {
-     const unsigned int high_lane_idx = w_extent - 1;
+     const std::uint32_t high_lane_idx = w_extent - 1;
 
      // send last lane value (total tile offset) to lane idx = low_lane_idx:
-     const warp::warp_mask_t active_mask = 1 | get_lane_mask(high_lane_idx);
-     const unsigned int tmp = warp::shfl_mask(acc, active_mask, local_offset, high_lane_idx, w_extent);
+     const mask_t active_mask = 1 | get_lane_mask<TAcc>(high_lane_idx);
+     const std::uint32_t tmp = warp::shfl(acc, active_mask, local_offset, high_lane_idx, w_extent);
 
      if (lane_idx == 0)
        local_offset = tmp;  //lane 0 keeps full (inclusive for the last lane) sum
@@ -177,13 +183,13 @@ namespace alpaka
 * @return Index of the lane in the mask 
 */
 
- template <alpaka::concepts::Acc TAcc>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE unsigned int get_logical_lane_idx(TAcc const& acc,
-                                                                  const warp::warp_mask_t mask,
-                                                                  const unsigned int lane_idx) {
+ template <concepts::Acc TAcc>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE std::uint32_t get_logical_lane_idx(TAcc const& acc,
+                                                                  typename TAcc::mask_type const mask,
+                                                                  std::uint32_t const lane_idx) {
    if (lane_idx == 0)
      return lane_idx;  // nothing to do, phys idx coincide with the logical one.
-   const warp::warp_mask_t lane_mask = mask & (get_lane_mask(lane_idx) - 1);
+   const auto lane_mask = mask & (get_lane_mask<TAcc>(lane_idx) - 1);
    return alpaka::popcount(acc, lane_mask);  // Count 1s below current lane
  }
 
@@ -199,23 +205,23 @@ namespace alpaka
 * @return Physical index of the lane in the mask 
 */
 
- template <alpaka::concepts::Acc TAcc>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE unsigned int get_physical_lane_idx(TAcc const& acc,
-                                                                   const warp::warp_mask_t mask,
-                                                                   int logical_lane_idx) {
-   using signed_warp_mask_t = std::make_signed_t<warp::warp_mask_t>;
+ template <concepts::Acc TAcc>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE std::uint32_t get_physical_lane_idx(TAcc const& acc,
+                                                                   typename TAcc::mask_type const mask,
+                                                                   std::int32_t logical_lane_idx) {
+   using signed_warp_mask_t = std::make_signed_t<typename TAcc::mask_type>;
 
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return 0;
 
-   signed_warp_mask_t m = mask;
+   signed_warp_mask_t m = static_cast<signed_warp_mask_t>(mask);
 
    while (logical_lane_idx--)
      m &= (m - 1);
 
    const auto pos = alpaka::ffs(acc, m);
 
-   return static_cast<unsigned int>(pos - 1);
+   return static_cast<std::uint32_t>(pos - 1);
  }
 
  /**
@@ -230,22 +236,22 @@ namespace alpaka
 * @return return reduced value (propagated to all lanes in the mask by default)
 */
 
- template <alpaka::concepts::Acc TAcc, typename reduce_t, typename reducer_t, bool all = true>
+ template <concepts::Acc TAcc, typename reduce_t, typename reducer_t, bool all = true>
    requires std::is_arithmetic_v<reduce_t>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE reduce_t warp_reduce(TAcc const& acc, reduce_t const in, const reducer_t f) {
-   constexpr unsigned int w_extent = get_warp_size<TAcc>();
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE auto warp_reduce(TAcc const& acc, reduce_t const in, const reducer_t f) -> reduce_t {
+   constexpr std::uint32_t w_extent = warp::getSizeCompileTime<TAcc>();
 
    reduce_t result = in;
 
    if constexpr (std::is_same_v<Device, alpaka::DevCpu>)
      return result;
 
-   for (unsigned int offset = w_extent / 2; offset > 0; offset /= 2) {
-     result = f(result, alpaka::warp::shfl_down(acc, result, offset, w_extent));
+   for (std::uint32_t offset = w_extent / 2; offset > 0; offset /= 2) {
+     result = f(result, warp::shfl_down(acc, result, offset, w_extent));
    }
 
    if constexpr (all)
-     result = alpaka::warp::shfl(acc, result, 0, w_extent);
+     result = warp::shfl(acc, result, 0, w_extent);
 
    return result;
  }
@@ -263,52 +269,52 @@ namespace alpaka
 * @return return reduced value (propagated to all lanes in the mask by default)
 */
 
- template <alpaka::concepts::Acc TAcc, typename reduce_t, typename reducer_t, bool all = true>
+ template <concepts::Acc TAcc, typename reduce_t, typename reducer_t, bool all = true>
    requires std::is_arithmetic_v<reduce_t>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE reduce_t warp_sparse_reduce(TAcc const& acc,
-                                                            const warp::warp_mask_t mask,
-                                                            const unsigned int lane_idx,
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE auto warp_sparse_reduce(TAcc const& acc,
+                                                            typename TAcc::mask_type const mask,
+                                                            std::uint32_t const lane_idx,
                                                             reduce_t const in,
-                                                            const reducer_t f) {
-   constexpr unsigned int w_extent = get_warp_size<TAcc>();
+                                                            const reducer_t f) -> reduce_t {
+   constexpr std::uint32_t w_extent = warp::getSizeCompileTime<TAcc>();
 
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return mask == 0 ? 0 : in;
 
    // Non-active lanes should skip the reduction:
-   if (is_work_lane(mask, lane_idx) == false)
+   if (is_work_lane<TAcc>(mask, lane_idx) == false)
      return in;
 
-   unsigned int nActiveLanes = alpaka::popcount(acc, mask);  // count number of active lanes
+   std::uint32_t nActiveLanes = alpaka::popcount(acc, mask);  // count number of active lanes
 
    // First check if this is just a single active lane in the warp:
    if (nActiveLanes == 1)
      return in;
 
    //Compute the next power of two:
-   const unsigned int pow2 = w_extent - cms::alpakaintrinsics::clz(acc, nActiveLanes - 1);
-   const unsigned int pow2_boundary = 1 << pow2;
+   const std::uint32_t pow2 = w_extent - warp::clz(acc, nActiveLanes - 1);
+   const std::uint32_t pow2_boundary = 1 << pow2;
 
-   const unsigned int logical_lane_idx = get_logical_lane_idx(acc, mask, lane_idx);
+   const std::uint32_t logical_lane_idx = get_logical_lane_idx(acc, mask, lane_idx);
 
    reduce_t res = in;
 
-   for (unsigned int offset = pow2_boundary / 2; offset > 0; offset /= 2) {
-     const unsigned int logical_src_lane_idx = logical_lane_idx + offset;
-     const unsigned int src_lane_idx =
+   for (std::uint32_t offset = pow2_boundary / 2; offset > 0; offset /= 2) {
+     const std::uint32_t logical_src_lane_idx = logical_lane_idx + offset;
+     const std::uint32_t src_lane_idx =
          (logical_src_lane_idx < nActiveLanes) ? get_physical_lane_idx(acc, mask, logical_src_lane_idx) : lane_idx;
 
-     const reduce_t neigh_res = warp::shfl_mask(acc, mask, res, src_lane_idx, w_extent);
+     const reduce_t neigh_res = warp::shfl(acc, mask, res, src_lane_idx, w_extent);
 
      if (logical_src_lane_idx < nActiveLanes)
        res = f(res, neigh_res);
 
-     warp::syncWarpThreads_mask(acc, mask);
+     warp::syncWarpThreads(acc, mask);
    }
 
    if constexpr (all) {
      const auto low_lane_idx = get_physical_lane_idx(acc, mask, 0);
-     res = warp::shfl_mask(acc, mask, res, low_lane_idx, w_extent);
+     res = warp::shfl(acc, mask, res, low_lane_idx, w_extent);
    }
 
    return res;
@@ -328,47 +334,47 @@ namespace alpaka
 * @return Exclusive prefix sum value for the current lane.
 */
 
- template <alpaka::concepts::Acc TAcc, bool all = true>
- ALPAKA_FN_ACC ALPAKA_FN_INLINE unsigned int warp_sparse_exclusive_sum(TAcc const& acc,
-                                                                       const warp::warp_mask_t mask,
-                                                                       const unsigned int val,
-                                                                       const unsigned int lane_idx) {
-   constexpr unsigned int w_extent = get_warp_size<TAcc>();
+ template <concepts::Acc TAcc, bool all = true>
+ ALPAKA_FN_ACC ALPAKA_FN_INLINE auto warp_sparse_exclusive_sum(TAcc const& acc,
+                                                                       typename TAcc::mask_type const mask,
+                                                                       std::uint32_t const val,
+                                                                       std::uint32_t const lane_idx) -> std::uint32_t {
+   constexpr std::uint32_t w_extent = warp::getSizeCompileTime<TAcc>();
 
    if constexpr (std::is_same_v<alpaka::Dev<TAcc>, alpaka::DevCpu>)
      return all == false ? 0 : (mask == 0 ? 0 : val);
 
    // Non-active lanes should skip the reduction:
-   if (is_work_lane(mask, lane_idx) == false)
+   if (is_work_lane<TAcc>(mask, lane_idx) == false)
      return 0;
 
    // count number of active lanes
-   const unsigned int nActiveLanes = alpaka::popcount(acc, mask);
+   const std::uint32_t nActiveLanes = alpaka::popcount(acc, mask);
    // First check if this is just a single active lane in the warp:
    if (nActiveLanes == 1)
      return val;  //nothing to do, note that this is the inclusive "sum": low lane always keeps the whole sum
 
    //Compute the next power of two:
-   const unsigned int pow2 = w_extent - cms::alpakaintrinsics::clz(acc, nActiveLanes - 1);
-   const unsigned int pow2_boundary = 1 << pow2;
+   const std::uint32_t pow2 = w_extent - warp::clz(acc, nActiveLanes - 1);
+   const std::uint32_t pow2_boundary = 1 << pow2;
 
-   const unsigned int logical_lane_idx = get_logical_lane_idx(acc, mask, lane_idx);
+   const std::uint32_t logical_lane_idx = get_logical_lane_idx(acc, mask, lane_idx);
 
-   unsigned int local_offset = val;
+   std::uint32_t local_offset = val;
 
-   for (unsigned int step = 1; step < pow2_boundary; step *= 2) {
-     const unsigned int src_lane_idx =
+   for (std::uint32_t step = 1; step < pow2_boundary; step *= 2) {
+     const std::uint32_t src_lane_idx =
          (logical_lane_idx >= step) ? get_physical_lane_idx(acc, mask, logical_lane_idx - step) : lane_idx;
-     const unsigned int tmp_val = warp::shfl_mask(acc, mask, local_offset, src_lane_idx, w_extent);
+     const std::uint32_t tmp_val = warp::shfl(acc, mask, local_offset, src_lane_idx, w_extent);
 
      if (logical_lane_idx >= step)
        local_offset += tmp_val;
    }
 
    if constexpr (all) {
-     const unsigned int high_lane_idx = get_physical_lane_idx(acc, mask, nActiveLanes - 1);
+     const std::uint32_t high_lane_idx = get_physical_lane_idx(acc, mask, nActiveLanes - 1);
      // send last lane value (total tile offset) to lane idx = low_lane_idx:
-     const unsigned int tmp = warp::shfl_mask(acc, mask, local_offset, high_lane_idx, w_extent);
+     const std::uint32_t tmp = warp::shfl(acc, mask, local_offset, high_lane_idx, w_extent);
 
      if (logical_lane_idx == 0)
        local_offset = tmp;  //lane 0 keeps full (inclusive for the last lane) sum
