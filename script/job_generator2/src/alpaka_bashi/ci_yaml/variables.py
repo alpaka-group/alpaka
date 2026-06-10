d5a29a764b3cdca0f2f6768de4dfbeb0a8bd9583
Simeon Ehrig <s.ehrig@hzdr.de>
2026-06-10 16:10:39 +0200

    use SM level 8.0 in every CUDA job

--- script/job_generator2/src/alpaka_bashi/ci_yaml/variables.py
+++ script/job_generator2/src/alpaka_bashi/ci_yaml/variables.py
@@ -120,12 +120,14 @@ def set_hipcc_variables(job_body: Dict[str, Any], combination: bashi.Combination
 @typechecked
 def get_sm_level(combination: bashi.Combination) -> str:
     """Get the CUDA SM level depending on the combination"""
+    # SM Level of the Nvidia Quadro P5000
+    # Compile also for the SM level even it is not used on the Quadro P500
+    nvidia_gpu_ci_runner_sm_level = "80"
+
     if combination[ALPAKA_ACC_GPU_CUDA_ENABLE].version < packaging.version.parse("13.0"):
         # SM Level of the Nvidia Quadro P5000
-        nvidia_gpu_ci_runner_sm_level = "61"
-    else:
-        # with CUDA 13.0 SM 6.1 is deprecated, therefore we use the SM Level of the Nvidia A100
-        nvidia_gpu_ci_runner_sm_level = "80"
+        # with CUDA 13.0 SM 6.1 is deprecated
+        nvidia_gpu_ci_runner_sm_level += ";61"
 
     if combination[JOB_EXECUTION_TYPE].version == JOB_EXECUTION_RUNTIME_VER:
         return nvidia_gpu_ci_runner_sm_level
